9d6087e1bcdaaccccf87b30148dcee63d1494698
Mateus Rodrigues de Morais <mateus.rodrigues@live.com>
2016-02-13 14:47:14 -0300

    Initial commit

---
+++ RodriguesLembra/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RodriguesLembra.Startup))]
+namespace RodriguesLembra
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
